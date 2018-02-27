000ac2da9bf448cbc0faa58fb6b1ec741b1df650
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-02-26 23:38:19 -0400

    refactor code

--- RomanNumerals/MyConverter.cs
+++ RomanNumerals/MyConverter.cs
@@ -1,22 +1,38 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 
 // https://remonsinnema.com/2011/12/05/practicing-tdd-using-the-roman-numerals-kata/
+// https://github.com/boucardbruno/RomanNumerals-Kata
 
 namespace RomanNumerals
 {
     public class MyConverter
     {
-        private static readonly int[] Values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-        private static readonly string[] RomanSymbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL" ,"X" ,"IX", "V", "IV", "I" };
+        private static readonly Dictionary<int, string> Symbols = new Dictionary<int, string>
+        {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1 , "I"}
+        };
 
         public static string ToRoman(int number)
         {
             StringBuilder result = new StringBuilder();
             int remaining = number;
 
-            for (int i = 0; i < Values.Length; i++)
+            foreach (var num in Symbols)
             {
-                remaining = AppendRomanNumerals(remaining, Values[i], RomanSymbols[i], result);
+                remaining = AppendRomanNumerals(remaining, num.Key, num.Value, result);
             }
 
             return result.ToString();
